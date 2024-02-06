c5407d6a7a85ea0682d92e4e8a01ddd9f7c7a0c4
kiran <bhatkiran74@gmail.com>
2024-02-07 00:54:35 +0530

    BankAppService: created maven project

---
+++ src/main/java/com/kiran/BankAppService/BankAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.BankAppService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/BankAppService/BankAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.BankAppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
