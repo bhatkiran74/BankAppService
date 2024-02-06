0a10ec30c52c95c971578bba23f61524f7ff5c9e
kiran <bhatkiran74@gmail.com>
2024-02-07 02:07:50 +0530

    BAS: implemented InMemoryUserDetailsManager

--- src/main/java/com/kiran/BankAppService/config/ProjectSecurityConfig.java
+++ src/main/java/com/kiran/BankAppService/config/ProjectSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,24 +17,34 @@ public class ProjectSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//        /* Access Denid for all */
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().denyAll())
+        http.authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/v1/myAccount","/api/vi/myBalance","/api/vi/myCards","/api/vi/myLoans").authenticated()
+                                .requestMatchers("/api/v1/contact","/api/vi/notices").permitAll()
+                                .anyRequest().authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-//        /* Access granted for all */
-//        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
-
-//        http.authorizeHttpRequests(authorize ->
-//                        authorize
-//                                .requestMatchers("/api/v1/myAccount","/api/vi/myBalance","/api/vi/myCards","/api/vi/myLoans").authenticated()
-//                                .requestMatchers("/api/v1/contact","/api/vi/notices").permitAll()
-//                                .anyRequest().authenticated()
-//                )
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("kiran")
+                .password("kiran123")
+                .authorities("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .authorities("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
