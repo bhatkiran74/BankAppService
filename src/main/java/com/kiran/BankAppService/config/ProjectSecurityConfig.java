ef80625ce78ad8a64b0033924a07b097bd1756c7
kiran <bhatkiran74@gmail.com>
2024-02-11 23:52:11 +0530

    BAS: implementation of our own custom UserDetailsService

--- src/main/java/com/kiran/BankAppService/config/ProjectSecurityConfig.java
+++ src/main/java/com/kiran/BankAppService/config/ProjectSecurityConfig.java
@@ -1,21 +1,13 @@
 package com.kiran.BankAppService.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,29 +26,6 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
-
-
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-//        UserDetails user = User.withUsername("kiran")
-//                .password("kiran123")
-//                .authorities("USER")
-//                .build();
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password("admin123")
-//                .authorities("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user,admin);
-//    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
---
+++ src/main/java/com/kiran/BankAppService/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.kiran.BankAppService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+    private String email;
+    private String pwd;
+    private String role;
+}
